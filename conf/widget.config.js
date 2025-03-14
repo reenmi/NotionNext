26f6da83af77190e375822b9a79a570a6fc26689
reenmi <ruca.n.Hallen@protonmail.com>
2025-03-14 15:04:58 +0800

    Add files via upload

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -37,11 +37,11 @@ module.exports = {
   MUSIC_PLAYER_AUDIO_LIST: [
     // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
     {
-      name: '风を共に舞う気持ち',
+      name: '悟空（笛子扬琴）',
       artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
+      url: 'https://music.163.com/song/media/outer/url?id=2625425764.mp3',
       cover:
-        'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+        'http://p2.music.126.net/k8LGUnmAVm4EdwDNMTlBlA==/109951169872323748.jpg'
     },
     {
       name: '王都グランセル',
